c3507d5954336714cbf00712e34d83c377af05f8
Ikendu <ndubest56@gmail.com>
2024-10-05 17:21:09 +0100

    more python

--- app.py
+++ app.py
@@ -1,22 +1,46 @@
-print("Hello world")
-age = 20
-print(age*10)
-sayIt="Igwekamma is working"
-print(sayIt)
-isTrue = True
-print(isTrue)
+# print("Hello world")
+# age = 20
+# print(age*10)
+# sayIt="Igwekamma is working"
+# print(sayIt)
+# isTrue = True
+# print(isTrue)
 
 # name = input("What is your name? ")
 # print("Great "+ name +" is with me")
 # birth_year = input("Your age year: ")
 # age = 2024 - int(birth_year)
 # print('your age is: ' + str(age))
-course = "Python"
+# course = "Python"
 # print(course.capitalize())
 
-if course == "Boy":
-    print("true")
-elif course == "Java":
-    print("true")
-else:
-    print("false")
+# if course == "Boy":
+#     print("true")
+# elif course == "Java":
+#     print("true")
+# else:
+#     print("false")
+
+# names = ["Igwekamma", "Chibundu", "Aniede", "Ndubueze"]
+
+# msg = f'{names[0]} is great'
+
+# print(msg)
+# print (len(names))
+# print("Aniede" in names)
+# print(names[1:3])
+
+# for name in names:
+#     print(name)
+
+# print('''
+# Hello Gift
+# How are u doing
+
+# I hope you are beautiful today 
+# ''')  
+
+i = 1
+while i < 10:
+    print(i * 'o')
+    i += 2
