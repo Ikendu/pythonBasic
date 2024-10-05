0e79f4ba2c9cb94a8f78baee2fb9dd43b8006a59
Ikendu <ndubest56@gmail.com>
2024-10-05 22:17:01 +0100

    other operations

--- others.py
+++ others.py
@@ -13,11 +13,19 @@ for num in nums:
         output += "x"
     print(output)
 
-nums = [2, 4, 5, 1, 8, 10, 5, 3]
+nums = [2, 4, 1, 5, 1, 8, 3, 10, 5, 3, 10]
 
+# get the maximum number
 max = nums[0]
 for num in nums:
     if num > max:
         max = num
 print(max)
+
+# remove duplicate
+unique = []
+for num in nums:
+    if num not in unique:
+        unique.append(num)
+print(unique)
 
