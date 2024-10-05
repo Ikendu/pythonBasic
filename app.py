49c6c0392a7d13e31472f1167ae5056205611e78
Ikendu <ndubest56@gmail.com>
2024-10-05 12:39:09 +0100

    basic python

---
+++ app.py
@@ -0,0 +1,22 @@
+print("Hello world")
+age = 20
+print(age*10)
+sayIt="Igwekamma is working"
+print(sayIt)
+isTrue = True
+print(isTrue)
+
+# name = input("What is your name? ")
+# print("Great "+ name +" is with me")
+# birth_year = input("Your age year: ")
+# age = 2024 - int(birth_year)
+# print('your age is: ' + str(age))
+course = "Python"
+# print(course.capitalize())
+
+if course == "Boy":
+    print("true")
+elif course == "Java":
+    print("true")
+else:
+    print("false")
