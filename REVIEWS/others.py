fba1cbc645d3036a30111aee81b63e17d9294b23
Ikendu <ndubest56@gmail.com>
2024-10-10 09:23:28 +0100

    looping

--- REVIEWS/others.py
+++ REVIEWS/others.py
@@ -1,38 +1,52 @@
-nums = [5, 2, 5, 2, 2]
+# nums = [5, 2, 5, 2, 2]
 
-# using easy python way
-for num in nums:
-    print("x" * num)
+# # using easy python way
+# for num in nums:
+#     print("x" * num)
 
 
-# using more traditional coding way
-print(" ")
-for num in nums:
-    output = ""
-    for inner in range(num):
-        output += "x"
-    print(output)
+# # using more traditional coding way
+# print(" ")
+# for num in nums:
+#     output = ""
+#     for inner in range(num):
+#         output += "x"
+#     print(output)
 
-nums = [2, 4, 1, 5, 1, 8, 3, 10, 5, 3, 10]
+# nums = [2, 4, 1, 5, 1, 8, 3, 10, 5, 3, 10]
 
-# get the maximum number
-max = nums[0]
-for num in nums:
-    if num > max:
-        max = num
-print(max)
+# # get the maximum number
+# max = nums[0]
+# for num in nums:
+#     if num > max:
+#         max = num
+# print(max)
 
-# remove duplicate
-unique = []
-for num in nums:
-    if num not in unique:
-        unique.append(num)
-print(unique)
+# for num in range(2, 30, 3):
+#     for x in range(5):
+#         print(num, x)
+    
+# else:
+#     print("Finished")
 
-# unpacking
-numbers = [20, 10, 5, 3, 60]
-x, y, z, a, b = numbers
-print(a)
+# # remove duplicate
+# unique = []
+# for num in nums:
+#     if num not in unique:
+#         unique.append(num)
+# print(unique)
 
-# Arrays, Tuple and Dictionary is closely related
+# # unpacking
+# numbers = [20, 10, 5, 3, 60]
+# x, y, z, a, b = numbers
+# print(a)
+
+# String, Arrays, Tuple and Dictionary is closely related
+word = "Igwekanma the God of my grand father will make me wealthy and powerful"
+
+if "my" in word:
+    print("Present")
+
+
+
 
