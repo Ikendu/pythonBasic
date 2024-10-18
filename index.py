df513cceccfa05ab09880db765b7ba43db55177f
Ikendu <ndubest56@gmail.com>
2024-10-18 15:02:04 +0100

    python basic

--- index.py
+++ index.py
@@ -11,16 +11,69 @@ print(intro)
 # age = input("Enter your age ")
 
 # print("Your are currently " + age + " year old")
+
+
 # score = input("Enter your score ")
 # finalResult = float(score) + 20
 # print("Your score plus your assessment is ")
 # print(finalResult)
 
 
-reply = "I am coming today"
+fullText = "today is " + "Friday"
+# print(fullText)
 
-print("am" in reply)
+part1 = "I am coming"
+part2 = "today or tomorrow "
+part3 = 200
+
+fullSentence = part1 + " " + part2
+
+finalSen = fullSentence + str(part3)
+
+print(finalSen)
+
+# print(part1 + part2)
+
+# multiLineText = """
+#    I am coding python
+# we are doing well
+# we are gradually covering up
+# but we just started
+#                 Thanks
+# """
+# print(multiLineText)
+
+# textWord = "some text here is getting long"
+
+# print(textWord[-4])
+
+# ans = textWord[3:]
+# print(ans)
 
 
+# print("The number of characters here is " +  str(len(multiLineText)))
+# print(f"The number of characters here is {part1} {part2} {part3}")
+
+# print(textWord.upper())
+# print(textWord.lower())
+# print(textWord.capitalize())
+
+# newList = textWord.split(" ")
+# print(newList)
+
+# print(5 == 3)
+
+# print(bool(["Amaka", "David"]))
+
+# print(4 < 2)
+
+num = 5
+num += 2
+num += 5
+num += 10
+
+print(num)
+
+# The number of characters here is 113
 
 
