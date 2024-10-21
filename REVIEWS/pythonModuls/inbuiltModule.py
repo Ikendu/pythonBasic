fb512e25467fecffd5c12398c31bf9edf8f6163a
Ikendu <ndubest56@gmail.com>
2024-10-21 12:37:08 +0100

    random number

--- REVIEWS/pythonModuls/inbuiltModule.py
+++ REVIEWS/pythonModuls/inbuiltModule.py
@@ -1,5 +1,13 @@
 import random
 
-for num in range(5):
-    print(random.randint(20, 50))
+# for num in range(5):
+#     print(random.randint(20, 50))
 
+
+class Dice:
+    def roll(self):
+        print(random.randint(1, 5), random.randint(1, 5))
+
+    
+roller1 = Dice()
+roller1.roll()    
