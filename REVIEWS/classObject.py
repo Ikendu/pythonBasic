f4e0206c0b2ffd2b2893e8c120fb21c6d7e1867c
Ikendu <ndubest56@gmail.com>
2024-10-21 09:45:46 +0100

    class in python

--- REVIEWS/classObject.py
+++ REVIEWS/classObject.py
@@ -1,15 +1,22 @@
 # Class can have method inside and attributes attach to objects created from the class
 
 class Point:
+    def __init__(self, value1, value2):
+        self.x = value1
+        self.y = value2
+        print(f"Ponit x is {self.x} and point y is {self.y}")
+
+
     def show(self):
         print("This is a point")
     def exit(self):
         print("Exiting right now...")
 
 
-point1 = Point()
+point1 = Point(20, 25)
 point1.show()
 point1.exit()
+
 point1.more = "More wealth coming..."
 log = point1.more
 print(log)
