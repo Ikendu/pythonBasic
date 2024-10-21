1a91e302fe93a9f15138f9cffa33ef2944a5d343
Ikendu <ndubest56@gmail.com>
2024-10-21 10:27:35 +0100

    class in python

--- REVIEWS/classObject.py
+++ REVIEWS/classObject.py
@@ -20,3 +20,37 @@ point1.exit()
 point1.more = "More wealth coming..."
 log = point1.more
 print(log)
+
+#Assign
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print("Defaul age is 18")
+        print(f"You name is {self.name}, You are {self.age} years old")
+
+    def talk(self):
+        print("I can say all things in Igbo language")
+
+person1 = Person("David", 30)
+person1.level = "Graduate"
+print(f"You are now a {person1.level}")
+
+# inheritance
+class Animal:
+    def __init__(self, name, legs, winds, hands, fur, feathers):
+        print(f"{name} has {legs} legs with {winds} wings, {hands} hands, {fur} Fur and {feathers} feathers")
+    
+    def noise(self):
+        print("I makes alot of noise")
+
+
+class Dog(Animal):
+    def how(self):
+        print("I bark a lot")
+
+dog1 = Dog("Chizzy", 4, 0, 0, "Plenty", 0)
+dog1.noise()
+
+
+
