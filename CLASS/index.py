881f9d71762359f769864911c3cc070583a9d8f9
Ikendu <ndubest56@gmail.com>
2024-11-07 15:50:43 +0100

    more on python

--- CLASS/index.py
+++ CLASS/index.py
@@ -1,1 +1,51 @@
-print("Hello world")
+intro = "Hello world"
+alpha = "A"
+
+num = 90
+
+num2 = "90"
+
+added = int(num2) + 10
+
+# converted = int(num2)
+
+print(added)
+
+
+
+# print(intro)
+
+# print(num)
+
+# print(intro)
+
+# print(intro)
+
+# print(intro)
+
+# print(intro)
+
+# print(num)
+
+result = num  + 20
+
+print(result)
+
+# items stores more than one data 
+items = ["orange1", "banna", "cashew", 100, 'a', 50]
+
+print(items[2])
+
+result2 = items[3] + 30
+
+print(result2)
+
+
+# let name =  "Udeh"
+# const name2 = "Rex"
+# var name3 = "Udeh Rex"
+
+# String name = "Udeh";
+# char letter = "A";
+# int num = 20;
+# float num2 = 30.5;
