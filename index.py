2ddc229afc72d03f11f4c8b2f4ac9c4a972b71dd
Ikendu <ndubest56@gmail.com>
2024-11-08 09:36:43 +0100

    more on python

--- index.py
+++ index.py
@@ -131,7 +131,7 @@
 
 fruit1 = "mango"
 fruit2 = 'orange'
-num = "30"
+num = 30
 
 basket = ["mango", "orange", "gova", "cashew", "banana", "mango", "gova", "banana"]
 
@@ -145,13 +145,107 @@ basket[1] = "book"
 basket[6] = "bread"
 
 
-print(num + 20 * 2)
-print(fruit1)
-print(answer)
-print(basket)
+# print(num + 20 * 2)
+# print(fruit1)
+# print(answer)
+# print(basket)
 # print(eating)
 # print(nebour)
 
+thislist = ["apple", "banana", "cherry"]
+
+print(thislist[2])
+
+thislist[2] = "Mango"
+
+print(thislist[2])
+
+fruits = ("apple", "banana", "cherry", "mango", "gova", "Casava")
+
+print(fruits)
+
+# (first, second, third, fourth,a,b ) = fruits
+
+# print(range(len(fruits)))
+# print(range(6))
+
+# for item in fruits:
+#     print(item)
+
+# for item in range(len(fruits)):
+#     print(fruits[item])
+
+# for(let x = 0; x < fruits.length; x++){
+#     print(fruits[x])
+# }
+
+fruits = {"first": "mango",
+           "second": "bannana", 
+           "third": "beans", 
+           "fourth":"mango", 
+           "fifth": {"one": "car", 
+                     "others": {"abc": 1000}, 
+                     "two": "house", 
+                     "three": "money"
+            },
+            "sixth": [20, 40, "boy", "baby"]
+}
+
+fruitsAb = fruits.copy()
+
+fruitsAb["abc"] = "Groundnut"
+
+print(fruits)
 
 
+fruits["first"] = "Gover"
+print(fruits["fifth"]["others"]['abc'])
+print(fruits['sixth'][2])
 
+
+# print(fruits["first"])
+
+
+myProps = fruits.keys()
+myValues = fruits.values()
+entries = fruits.items()
+potOfBeans = fruits["third"]
+changed = fruits.update({"second": "Oranges"})
+
+# print((fruits))
+
+# fruits["fifth"] = "Pawpaw"
+# # fruits.pop('fourth')
+
+
+# print(fruits)
+
+# print(myValues)
+# print(myProps)
+# print(entries)
+# print(potOfBeans)
+
+# for x in fruits.items():
+#     print(x)
+
+
+item1 = 20
+item2 = 30
+
+if item1 < item2:
+    print("It is greater")
+    print("We are matching")
+    print("Good job so far")
+    if 2 < 5:
+        print("wow we got inside the first if statement")
+elif item1 > item2 and item1 < item2:
+    print("It is geater")
+    print("Another printout")
+elif item1 >= item2:
+    print("We have found the result")
+else:
+    value = 50 + 20
+    print("It is not true")
+    print(value)
+
+print("Hello world")
