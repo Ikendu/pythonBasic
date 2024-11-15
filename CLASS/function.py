dbdb76193ebafd70ab1aff4c14743aa2bbba039c
Ikendu <ndubest56@gmail.com>
2024-11-15 13:02:05 +0100

    python funtion

--- CLASS/function.py
+++ CLASS/function.py
@@ -13,4 +13,19 @@ def addScore(value, name):
     result = value + 30
     print(name + " scored " + str(result))
 
+def myStudents(*student):
+    print(f"first name is {student[0]}")
+    print(f"second name is {student[1]}")
+    print(f"third name is {student[2]}")
+    print(f"last name is {student[-1]}")
+
 addScore(49, "John Man")
+addScore(50, "David")
+
+myStudents('John', 'Mary', 'Mich', 'Juliet', 'Gift', 'Akuoma', 'Excel')
+
+def addition(num1=10, num2=20):
+    result = num1 + num2
+    print(result)
+
+addition(40, 20)
