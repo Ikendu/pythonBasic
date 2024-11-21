b8c0af59726e4e81eb25f5f856bac8eb639c0f19
Ikendu <ndubest56@gmail.com>
2024-11-21 21:05:42 +0100

    more on python

--- CLASS/class.py
+++ CLASS/class.py
@@ -12,17 +12,29 @@ class Add:
     second = 20
     third = 30
 
-    def __init__(self, a, b, c):
-        self.num1 = a
-        self.num2 = b
-        self.num3 = c
+    def __init__(right, a, b, c):
+        right.a = a
+        right.b = b
+        right.c = c
+    
+    def add(self):
+        result = self.a + self.b + self.c
+        # print(result)
+        return result
+    
+    def shouting(self):
+        print("Hello my good friend")
+
+
 
 nums = Add(20, 30, 40)
 nums1 = Add(100, 200, 300)
 nums2 = Add(30, 50, 60)
 nums3 = Add(1000, 2000, 3000)
 
-print(nums2.third)
+print(nums2.shouting())
+print(nums2.add())
+
 
 # nums = Add(10, 20, 30)
 # myObj = Add(100, 200, 50)
@@ -34,4 +46,79 @@ print(nums2.third)
 # print(myObj.num3 + final)
 
 # obj2.addition()
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+
+class Candidate(Person):
+   def __init__(self, fname, lname, address):
+      super().__init__(fname, lname)
+      self.address = address
+
+   def reverse(self):
+      print(self.lastname, self.firstname)
+
+
+student1 = Person("Ada", "Adaja")
+student2 = Candidate("Obi", 'Obinna', "Oppo UNN gate")
+
+student1.printname()
+# student1.reverse()
+student2.printname()
+student2.reverse()
+print(student2.address)
+
+listItems = ["chair", 'bed', 'table', 'cup', 'curtine']
+
+getter = iter(listItems)
+
+print(next(getter))
+print(next(getter))
+print(next(getter))
+print(next(getter))
+print(next(getter))
+
+num = 50
+num2 = 100
+
+def show(): 
+   xy = 40  
+   print(num + num2, xy)
+
+   def moreShow():
+      global yz
+      yz = 100
+      print(yz, xy, num, num2)
+      
+   print(num, num2, xy)
+
+   return moreShow()
+ 
+
+
+show()
+
+print(num + num2)
+print(yz)
+
+# import  function
+import index
+from function import multiply
+import datetime
+
+multiply(30, 20)
+
+print(index.intro)
+print(index.items[0])
+
+time = datetime.datetime.now()
+print(time.second)
+
+
 
