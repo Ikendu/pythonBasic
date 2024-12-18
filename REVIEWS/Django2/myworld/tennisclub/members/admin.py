4886353946830c7eb5b1200dbd9ab40f6d6aa867
Ikendu <ndubest56@gmail.com>
2024-12-18 17:22:59 +0100

    more on django rendering

--- REVIEWS/Django2/myworld/tennisclub/members/admin.py
+++ REVIEWS/Django2/myworld/tennisclub/members/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Member
 
 # Register your models here.
+# admin.site.register(Member)
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname", 'phone', 'date')
+
+admin.site.register(Member, MemberAdmin)
+
+
--- REVIEWS/Django2/myworld/tennisclub/members/models.py
+++ REVIEWS/Django2/myworld/tennisclub/members/models.py
@@ -8,3 +8,5 @@ class Member(models.Model):
     phone = models.IntegerField(null=True)
     date = models.DateField(null=True)
     
+    # def __str__(self):
+    #     return f"{self.firstname} {self.lastname} {self.phone} {self.date}"
