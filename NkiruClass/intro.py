8364ac23eaaaf5a81de909ea254b7e76bcdcc10e
Chibundu <ndubest56@gmail.com>
2025-01-30 11:21:00 +0100

    intro py

---
+++ NkiruClass/intro.py
@@ -0,0 +1,27 @@
+# print("Hello New to Coding")
+print("I am learning Python")
+
+# the description of my car
+# we just start python today 30-01-2025
+# Intoduction
+
+myCar = "Toyota Camry"
+myCarYear = 2015
+numStr = "2030"
+myNum = 34.5
+
+sum = myCarYear + myNum
+
+
+
+print(type(myCar))
+print(type(myCarYear))
+print(type(myNum))
+myPro = myCar + " House"
+
+print(myCar)
+print(myCarYear)
+print(numStr)
+print(sum)
+
+# print(myPro)
