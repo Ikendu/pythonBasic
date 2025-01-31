1f0712e1f7cf2976b7f8951387631a04286005c9
Chibundu <ndubest56@gmail.com>
2025-01-31 11:10:19 +0100

    more on python

--- NkiruClass/intro.py
+++ NkiruClass/intro.py
@@ -1,29 +1,73 @@
 # print("Hello New to Coding")
-print("I am learning Python")
+# print("I am learning Python")
 
 # the description of my car
 # we just start python today 30-01-2025
 # Intoduction
 
-myCar = "Toyota Camry"
-myCarYear = 2015
-numStr = "2030"
-myNum = 34.5
+# myCar = "Toyota Camry"
+# myCarYear = 2015
+# numStr = "2030"
+# myNum = 34.5
 
-sum = str(myCarYear) + numStr
-sum1 = myCarYear + int(numStr)
+# sum = str(myCarYear) + numStr
+# sum1 = myCarYear + int(numStr)
 
 
 
-print(type(myCar))
-print(type(myCarYear))
-print(type(myNum))
-myPro = myCar + " House"
+# print(type(myCar))
+# print(type(myCarYear))
+# print(type(myNum))
+# myPro = myCar + " House"
 
-print(myCar)
-print(myCarYear)
-print(numStr)
-print(sum)
-print(sum1)
+# print(myCar)
+# print(myCarYear)
+# print(numStr)
+# print(sum)
+# print(sum1)
+
+"""Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
+myStr = "hello world consectetur adipiscing elit,sed do eivhjjjfghjghjh fhjgffg fgjfghjfgj fjnfghjfgjfgjfgfd hthfgjgfhjusmod tempor incididuntut labore et dolore magna aliqu"
+myList = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango", "apple", "banana", "cherry", "orange", "kiwi", "melon", "mango","apple", "banana", "cherry", "orange", "kiwi", "melon", "mango","apple", "banana", "cherry", "orange", "kiwi", "melon", "mango",]
+
+print(myStr)
+print(myStr[1])
+print(myStr[2:7])
+print(myStr[-3])
+
+print(myList)
+print(myList[0])
+print(myList[:], "ghfghfh")
+print(myList[-3])
+
+strLenght = len(myStr)
+listLenght = len(myList)
+print(strLenght)
+print(listLenght)
+
+
+# for x in myList:
+#     print(x)
+
+
+
+num = 10
+num += 5
+
+num2 = 5
+result = num + num2
+
+
+
+
+
+
+
+
+
 
 # print(myPro)
