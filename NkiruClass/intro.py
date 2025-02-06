66e1ee89737f6b1bafeb93e5da133af02bc6c897
Chibundu <ndubest56@gmail.com>
2025-02-06 10:58:51 +0100

    python script

--- NkiruClass/intro.py
+++ NkiruClass/intro.py
@@ -34,34 +34,76 @@ ut labore et dolore magna aliqua."""
 myStr = "hello world consectetur adipiscing elit,sed do eivhjjjfghjghjh fhjgffg fgjfghjfgj fjnfghjfgjfgjfgfd hthfgjgfhjusmod tempor incididuntut labore et dolore magna aliqu"
 myList = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango", "apple", "banana", "cherry", "orange", "kiwi", "melon", "mango","apple", "banana", "cherry", "orange", "kiwi", "melon", "mango","apple", "banana", "cherry", "orange", "kiwi", "melon", "mango",]
 
-print(myStr)
-print(myStr[1])
-print(myStr[2:7])
-print(myStr[-3])
+# print(myStr)
+# print(myStr[1])
+# print(myStr[2:7])
+# print(myStr[-3])
 
-print(myList)
-print(myList[0])
-print(myList[:], "ghfghfh")
-print(myList[-3])
+# print(myList)
+# print(myList[0])
+# print(myList[:], "ghfghfh")
+# print(myList[-3])
 
-strLenght = len(myStr)
-listLenght = len(myList)
-print(strLenght)
-print(listLenght)
+# strLenght = len(myStr)
+# listLenght = len(myList)
+# print(strLenght)
+# print(listLenght)
 
 
 # for x in myList:
 #     print(x)
 
+# if "world" in myStr:
+#     print("Yes, 'world' is present in the following sentence")
+#     print("We are ready")    
+# else:
+#     print("No, 'world' is not present in the following sentence")
+
+button = "blue" 
+
+if button == "blue":
+    print("Start the car")
+elif button == "green":
+    print("On the AC")  
+elif button == "pink":
+    print("On the Television")
+elif button == "black":
+    print("On the Radio")    
+elif button == "red":
+    print("Stop the car")
+else: 
+    print("the car is parked")   
 
 
-num = 10
-num += 5
+thislist = ["apple", "banana", "cherry", "Mangoes"]
+tropical = ["mango", "pineapple", "papaya"]
+# print(thislist)
+# thislist[0] = "Mellon"
+# thislist[1] = "Orange"
+# thislist.append("Pineapple")
+# thislist.append("Grapes")
+# thislist.insert(3, "Strawberry")
+# tropical.extend(thislist)
+# thislist.pop(2)
+# thislist.clear()
+# print(thislist)
 
-num2 = 5
-result = num + num2
+thislist = ["apple", "banana", "cherry", "Mangoes"]
+newList = []
+print(newList)
 
+# for x in thislist:
+#     if 'e' in x:
+#         print(x)
+#     else:
+#         print("No e in " + x)    
 
+for x in thislist:
+    if 'a' in x:
+        newList.append(x)
+
+print(newList)  
+# newList =[ x for x in thislist if 'a' in x]      
 
 
 
